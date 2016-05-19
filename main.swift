4320b1cc06e6529d09c1f279c921716b92f8533d
cuijianhang <a5759@mac25.local>
2016-05-19 11:02:47 +0800

    1
    1

---
+++ main.swift
@@ -0,0 +1,59 @@
+//
+//  main.swift
+//  test1
+//
+//  Created by 5759 on 16/3/3.
+//  Copyright © 2016年 5759. All rights reserved.
+//
+
+/*
+import Foundation
+
+print("Hello, World!")
+
+
+var a:Int,b:Int,c:Int
+a=100
+b=200
+c=a+b
+print("\(a)+\(b)=\(c)")
+
+
+for ch in "hello".characters{
+    print(ch)
+}
+*/
+
+
+
+/*
+import Cocoa
+var i:Int
+for i=1;i<10;i++ {
+    print("\(i)")
+}
+
+func hello(){
+    print("hello!!")
+}
+hello()
+
+func add(x:Int,y:Int){
+    print("\(x)+\(y)=\(x+y)")
+}
+//参数必须指出数值且不能写出第一个
+add(10,y:20)
+
+func add(o:Int,p:Int,q:Int){
+    print("\(o)+\(p)+\(q)+\(o+p+q)")
+}
+add(10,p:20,q:30)
+*/
+
+import Foundation
+var me = ("CuiJIanhang",22,"378931184@qq.com")
+me.0
+me.1
+me.2
+
+
